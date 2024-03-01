89423b4e6e795447c96702231fb3df3fac0a87e2
Roland Halász <roland.halasz@outlook.com>
2024-03-01 22:35:46 +0100

    feat: add model for list items

---
+++ src/model/ListItem.ts
@@ -0,0 +1,38 @@
+export interface Item {
+  id: string;
+  item: string;
+  checked: boolean;
+}
+
+export default class ListItem implements Item {
+  constructor(
+    private _id = '',
+    private _item = '',
+    private _checked = false
+  ) {
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  set id(id: string) {
+    this._id = id;
+  }
+
+  get item() {
+    return this._item;
+  }
+
+  set item(item: string) {
+    this._item = item;
+  }
+
+  get checked() {
+    return this._checked;
+  }
+
+  set checked(checked: boolean) {
+    this._checked = checked;
+  }
+}
